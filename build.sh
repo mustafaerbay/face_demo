12ea91ddaa881ab3aa01b4674f82bc9e2370b2a9
mustafa erbay m00483517 <>
2022-02-18 12:06:11 +0300

    update

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 TAG=0.1.$CIRCLE_BUILD_NUM
 docker build -t circleci-demo-docker:$TAG .
 echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
-docker push CircleCI-Public/circleci-demo-docker:$TAG
+docker push circleci-demo-docker:$TAG
 
 # $ docker run --rm -it -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.17 bash
 # $ for GOOS in darwin linux; do
