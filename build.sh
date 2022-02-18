42223e4a079c8b33003729659a431c7e1c6dd9f0
mustafa erbay m00483517 <>
2022-02-18 11:55:14 +0300

    build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+TAG=0.1.$CIRCLE_BUILD_NUM
+docker build -t CircleCI-Public/circleci-demo-docker:$TAG .
+echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+docker push CircleCI-Public/circleci-demo-docker:$TAG
+
+# $ docker run --rm -it -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.17 bash
+# $ for GOOS in darwin linux; do
+# >   for GOARCH in 386 amd64; do
+# >     export GOOS GOARCH
+# >     go build -v -o myapp-$GOOS-$GOARCH
+# >   done
+# > done
