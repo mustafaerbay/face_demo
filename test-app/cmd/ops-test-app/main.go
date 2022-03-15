b79c3a3e77750aef531d8518de7a6e07c142acf7
mustafaerbay <mustafaerbay365@gmail.com>
2022-03-15 16:28:32 +0300

    uppp

--- test-app/cmd/ops-test-app/main.go
+++ test-app/cmd/ops-test-app/main.go
@@ -67,7 +67,7 @@ func run() error {
 		}
 		logrus.Info("health endpoint is ok")
 		res.WriteHeader(http.StatusOK)
-		_, _ = fmt.Fprintf(res, "UPP\n")
+		_, _ = fmt.Fprintf(res, "UPPP\n")
 	})
 
 	// listen and serve
